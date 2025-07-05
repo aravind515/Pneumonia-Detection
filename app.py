ba0bcdf7cb773c3454d5f1732e09cf507f21373a
aravind515 <aravind@techgramam.com>
2025-07-05 11:43:45 +0530

    dyanamic port allocation

--- app.py
+++ app.py
@@ -96,8 +96,8 @@ def predict():
         "confidence": round(confidence, 2)
     })
 
-# -----------------------------
-# Run App
-# -----------------------------
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000)
+port = int(os.environ.get("PORT", 5000))
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=port)
+
